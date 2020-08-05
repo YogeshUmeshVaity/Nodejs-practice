195da16c7772ed7b0e47cce7f9ef2cb542400385
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2020-08-05 18:39:29 +0530

    Use modules to move and request handler function in another file.

--- app.js
+++ app.js
@@ -1,43 +1,6 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes')
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === '/') {
-    res.write('<html>');
-    res.write('<head><title>Enter Message</title><head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-    res.write('</html>');
-    return res.end();
-  }
-  if (url === '/message' && method === 'POST') {
-    const body = []; 
-    // on() allows us to listen for certain events. We listen to 'data' event here.
-    // The 'data' event is fired whenever a new chunk is ready to be read.
-    // We specify what to do with the chunk in the listener code.
-    req.on('data', (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    // 'end' event will fire once the it is done parsing the incoming request. 
-    // We are using this to make sure we have a complete incoming data in the body array.
-    // Now we work with the chunks of the data that we collected.
-    req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1]; // e.g. message=Welcome we need the 'Welcome' part here.
-      fs.writeFileSync('message.txt', message);
-    });
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
-  }
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>My First Page</title><head>');
-  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-  res.write('</html>');
-  res.end();
-});
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
